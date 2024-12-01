cd67221a5af6d19e7cc873133a9457b6c6cad05f
Yair Steinlauf <yair.steinlauf@gmail.com>
2024-12-01 17:41:02 +0200

    some updates

--- include/Controller.h
+++ include/Controller.h
@@ -4,16 +4,18 @@
 #include "Player.h"
 #include "Guard.h"
 #include "vector"
+#include "Bomb.h"
 
 class Controller {
 public:	
 	Controller();
 	void run();
-	void loadBoard();
+	void playBoard(Board& board);
 	
 private:
 	Board m_board;
-	Player m_player;
-	std::vector<Guard> m_guard;
+	//Player m_player;
+	//std::vector<Guard> m_guard;
+	//std::vector<Bomb> m_bombs;
 	
 };
